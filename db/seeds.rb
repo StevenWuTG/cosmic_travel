772597ad8619f307bc722cba78801dc23e7424d2
StevenWuTG <stevenwu91@icloud.com>
2020-11-04 22:14:19 -0500

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-# Mission.destroy_all
+puts "destroying seeds"
+Mission.destroy_all
 Planet.destroy_all
-# Scientist.destroy_all
+Scientist.destroy_all
 
 planet_images = [
   "https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2003/04/martian_poles_and_cloud_cover/9834222-3-eng-GB/Martian_poles_and_cloud_cover_pillars.jpg",
@@ -22,15 +22,17 @@ planet_images = [
   "https://www.popsci.com/resizer/h_Wb0PwWbHG8pyy-57RGJ563bqo=/1034x1034/arc-anglerfish-arc2-prod-bonnier.s3.amazonaws.com/public/RZODSOHKA6O4VMNPPUW7GUTUBE.jpg",
   "https://www.popsci.com/resizer/pvibP816CN_tzyv2o9q1V_0kXUI=/1034x715/arc-anglerfish-arc2-prod-bonnier.s3.amazonaws.com/public/N3OYC3VMO5GPI2B5WQ733PLNNE.jpg"
 ]
-
+puts "making new seeds"
 20.times {Planet.create(name: Faker::TvShows::StarTrek.unique.location,
                         distance_from_earth: Faker::Space.unique.distance_measurement,
                         nearest_star: Faker::Space.star,
                         planet_img: planet_images.sample)}
 
-# 15.times {Scientist.create(name: Faker::FunnyName.name,
-#                            field_of_study: Faker::Educator.subject)}
+15.times {Scientist.create(name: Faker::FunnyName.name,
+                            field_of_study: Faker::Educator.subject)}
 
-# 20.times {Mission.create(name: Faker::TvShows::Buffy.unique.episode,
-#                          scientist_id: Scientist.all.sample.id, 
-#                          planet_id: Planet.all.sample.id)}
+ 20.times {Mission.create(name: Faker::TvShows::Buffy.unique.episode,
+                          scientist_id: Scientist.all.sample.id, 
+                          planet_id: Planet.all.sample.id)}
+
+puts "done"
